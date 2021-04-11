f3b7543ac5e62025bf869e4d724b4c695703b234
A tech group <77225190+hacke777@users.noreply.github.com>
2021-04-11 16:26:12 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -22,7 +22,7 @@ COLLECTION_NAME = 'channel_files'  # If you are using the same database, then us
 
 # Messages
 START_MSG = """
-**Hi, I'm Media Search**
+**Hi, I'm Media Search bot**
 
 Here you can search files in inline mode. Just press follwing buttons and start searching.
 """
